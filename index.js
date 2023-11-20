461fa91d17ee80d87591baa4f2e31711c822c180
Aayush Krishnan <ayush05012003krishn@gmail.com>
2023-11-21 01:07:01 +0530

    cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-app-frontend-eight-phi.vercel.app/login",
         credentials: true,
     },
 });
